5185e7db13bf4d4f43a010545f027430680e3218
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-20 14:36:44 +0300

    page-count-setters-getters

--- lib/book.py
+++ lib/book.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self,title, page_count):
+    def __init__(self, title, page_count):
         self.title = title
-        self.page_count = page_count
+        self._page_count = page_count  # internal storage
 
+    @property
+    def page_count(self):
+        return self._page_count
 
-    
-        
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")        
