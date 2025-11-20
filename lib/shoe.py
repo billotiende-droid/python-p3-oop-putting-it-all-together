439c612cc8d6e6ef86db729207e8d499756d6697
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-20 15:17:00 +0300

    size-int-instance

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-
 class Shoe:
-    def __init__(self,brand,size):
+    def __init__(self, brand, size):
         self.brand = brand
-        self.size = size
+        self._size = size  #Note : use _size so the setter is not triggered in __init__
 
+    @property
+    def size(self):
+        return self._size
 
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
     
