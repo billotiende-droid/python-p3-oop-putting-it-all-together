c1df9b2d06518d7754d6c00c8c9a0f05a6ab9b68
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-20 15:22:45 +0300

    cobble-method-constructor

--- lib/shoe.py
+++ lib/shoe.py
@@ -14,4 +14,7 @@ class Shoe:
             self._size = value
         else:
             print("size must be an integer")
-    
+
+    def cobble(self):
+        self.condition = "New" 
+        print("Your shoe is as good as new!")       
